0b0c40bd993829d3de7268af8aea0d23464adda6
pstraw <github_main@pstrawberrie.live>
2025-07-25 09:50:57 -0500

    continue foundation

---
+++ src/components/Hero.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let { title } = $props();
+</script>
+
+<div class="hero">
+  <div class="box">
+    <div class="hero_text">
+      <h1>{title}</h1>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  @use '@styles/util';
+
+  .hero {
+    position: relative;
+    display: flex;
+    align-items: center;
+    background-color: var(--c-primary);
+    padding: var(--space-page-v) var(--space-page-h);
+  }
+</style>
