8ae1256df8c8b7ce376aade11a34aa7a370c4ed4
pstraw <github_main@pstrawberrie.live>
2025-07-30 09:03:14 -0500

    add html bg color, remove current hero stars for a redo

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -24,26 +24,7 @@
     {/if}
   </div>
   {#if feature === 'stars'}
-    <div class="hero-stars">
-      <div class="glow-star star-1"></div>
-      <div class="glow-star star-2"></div>
-      <div class="glow-star star-3"></div>
-      <div class="glow-star star-4"></div>
-      <div class="glow-star star-5"></div>
-      <div class="glow-star star-6"></div>
-      <div class="glow-star star-8"></div>
-      <div class="glow-star star-9"></div>
-      <div class="glow-star star-10"></div>
-      <div class="glow-star star-11"></div>
-      <div class="glow-star star-12"></div>
-      <div class="glow-star star-13"></div>
-      <div class="glow-star star-15"></div>
-      <div class="glow-star star-16"></div>
-      <div class="glow-star star-17"></div>
-      <div class="glow-star star-18"></div>
-      <div class="glow-star star-19"></div>
-      <div class="glow-star star-20"></div>
-    </div>
+    @TODO stars
   {/if}
 </div>
 
